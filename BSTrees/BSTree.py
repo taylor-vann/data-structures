4765b1f194525a4666328cbca00946b82069b58b
Brian Taylor Vann <brian.taylor.vann@gmail.com>
2017-03-19 02:09:49 -0400

    BSTree insert and search

--- BSTrees/BSTree.py
+++ BSTrees/BSTree.py
@@ -14,14 +14,61 @@ Methods:
 - remove(<data>)
 """
 
+from BSTNode import BSTNode
+
+
 class BSTree(object):
 
-    #overwritten methods
-    def __init__(self):
-        pass
+    _root = None
 
-    def __contains__(self):
-        pass
+    #overwritten methods
+    def __init__(self, *args):
+        for var in args:
+            self.insert(var)
+
+
+    def __contains__(self, bit):
+        return self.search(bit)
 
 
     #custom methods
+    def insert(self, bit):
+        if self._root == None:
+            self._root = BSTNode(bit)
+            return
+
+        p = None
+        c = self._root
+
+        while c != None:
+            print(c.get_data())
+            if c.get_data() == bit:
+                return
+
+            if c.get_data() > bit:
+                p = c
+                c = c.get_left()
+            else:
+                p = c
+                c = c.get_right()
+ 
+        if p.get_data() > bit:
+            p.set_left(BSTNode(bit))
+        else:
+            p.set_right(BSTNode(bit))
+
+
+    def search(self, bit):
+        node = self._root
+
+        while node != None:
+            if node.get_data() == bit:
+                return node
+
+            if node.get_data() > bit:
+                node = node.get_left()
+
+            if node.get_data() < bit:
+                node = node.get_right()
+
+        return None
--- BSTrees/TestBSTree.py
+++ BSTrees/TestBSTree.py
@@ -16,4 +16,47 @@ from BSTree import BSTree
 class TestBSTreeMethods(unittest.TestCase):
 
     def testBSTreeIsNotNone(self):
-        pass
+        bstree = BSTree()
+        self.assertIsNotNone(bstree) 
+
+
+    def testBSTreeRootNotNone(self):
+        bstree = BSTree()
+        bstree.insert(1)
+        self.assertIsNotNone(bstree.search(1))
+
+
+    def testBSTreeContains(self):
+        bstree = BSTree()
+        bstree.insert(1)
+        self.assertIn(1, bstree)
+
+
+    def testBSTreeNotContains(self):
+        bstree = BSTree()
+        bstree.insert(1)
+        self.assertNotIn(2, bstree)
+
+
+    def testBSTreeInsert2Contains(self):
+        bstree = BSTree()
+        bstree.insert(1)
+        bstree.insert(2)
+        self.assertIn(2, bstree)
+
+
+    def testBSTreeInsert5Contains(self):
+        bstree = BSTree(4, 3, 6)
+        bstree.insert(5)
+        bstree.insert(1)
+        self.assertIn(5, bstree)
+
+
+    def testBSTreeInsert5NotContains(self):
+        bstree = BSTree(4, 3, 6)
+        bstree.insert(5)
+        bstree.insert(1)
+        self.assertNotIn(2, bstree)
+
+
+unittest.main()
