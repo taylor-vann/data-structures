d564148d707ab29268f946b4f873c2a59c6a4d5b
Taylor Vann <brian.taylor.vann@gmail.com>
2018-03-02 15:44:11 -0800

    Make Heapsort readable and remove comments

--- Python3/Sorts/HeapSort.py
+++ Python3/Sorts/HeapSort.py
@@ -1,48 +1,35 @@
 """
 Brian Taylor Vann
 github.com/taylor-vann
-
-Description:
-- Module for heap sort
-
-Requirements:
-- None
-
-Methods:
-- heap_sort
-- sift_down
 """
 
-def heap_sort(a):
-    l = len(a)
+def heap_sort(arr):
+    length = len(arr)
 
-    if l < 2:
-        return a
+    if length < 2:
+        return arr
 
-    # from last parent to first, sift down to make largest value at the top
-    for j in range(l // 2, -1, -1):
-        sift_down(a, j, l)
+    for j in range(length // 2, -1, -1):
+        sift_down(arr, j, length)
 
-    # swap largest and smallest value and sift down
-    for k in range(l - 1, -1, -1):
-        a[0], a[k] = a[k], a[0]
-        sift_down(a, 0, k)
+    for k in range(length - 1, -1, -1):
+        arr[0], arr[k] = arr[k], arr[0]
+        sift_down(arr, 0, k)
 
-    # send list, half of length, and length to heapify
-    return a
+    return arr
 
 
-def sift_down(a, i, l):
-    c = i
-    n = 2 * c + 1
+def sift_down(arr, start, finish):
+    curr = start
+    nxt = 2 * curr + 1
 
-    while n < l:
-        if n < l - 1 and a[n] < a[n + 1]:
-            n += 1
+    while nxt < finish:
+        if nxt < finish - 1 and arr[nxt] < arr[nxt + 1]:
+            nxt += 1
 
-        if a[c] < a[n]:
-            a[c], a[n] = a[n], a[c]
-            c = n
-            n = c * 2 + 1
+        if arr[curr] < arr[nxt]:
+            arr[curr], arr[nxt] = arr[nxt], arr[curr]
+            curr = nxt
+            nxt = curr * 2 + 1
         else:
             break
