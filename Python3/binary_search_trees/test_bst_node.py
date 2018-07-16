ce63488675d03224b280adb3c34909c9980a11a5
taylor-vann <brian.taylor.vann@gmail.com>
2018-07-16 03:05:23 -0700

    Move traversals into binary search trees

--- Python3/binary_search_trees/test_bst_node.py
+++ Python3/binary_search_trees/test_bst_node.py
@@ -9,57 +9,57 @@ from binary_search_tree import BSTNode
 
 class TestBSTNode(unittest.TestCase):
 
-    def testBSTNodeIsNotNone(self):
+    def test_bst_node_is_not_none(self):
         node = BSTNode()
         self.assertIsNotNone(node)
 
 
-    def testBSTNodeDataIsNotNone(self):
+    def test_bst_node_data_is_not_none(self):
         node = BSTNode(3)
         self.assertIsNotNone(node.data)
 
 
-    def testBSTNodeDataIsEqual(self):
+    def test_bst_node_data_is_equal(self):
         node = BSTNode(3)
         self.assertEqual(node.data, 3)
 
 
-    def testBSTNodeDataRightIsNone(self):
+    def test_bst_node_data_right_is_none(self):
         node = BSTNode(3, None, None)
         self.assertIsNone(node.right)
 
 
-    def testBSTNodeDataLeftIsNone(self):
+    def test_bst_node_data_left_is_none(self):
         node = BSTNode(3, None, None)
         self.assertIsNone(node.right)
 
 
-    def testBSTNodeSetData(self):
+    def test_bst_node_set_data(self):
         node = BSTNode()
         node.data = 2
         self.assertEqual(node.data, 2)
 
 
-    def testBSTNodeSetLeftNone(self):
+    def test_bst_node_set_left_none(self):
         node = BSTNode()
         node.left = None
         self.assertIsNone(node.left)
 
 
-    def testBSTNodeSetLeft(self):
+    def test_bst_node_set_left(self):
         node1 = BSTNode()
         node2 = BSTNode()
         node1.left = node2
         self.assertEqual(node1.left, node2)
 
 
-    def testBSTNodeSetRightNone(self):
+    def test_bst_node_set_right_none(self):
         node = BSTNode()
         node.right = None
         self.assertIsNone(node.right)
 
 
-    def testBSTNodeSetRight(self):
+    def test_bst_node_set_right(self):
         node1 = BSTNode()
         node2 = BSTNode()
         node1.right = node2
