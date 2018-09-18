d2eb1f8433bce472fa71e733159f90eb5c43e809
Brian Vann <brian.taylor.vann@gmail.com>
2018-09-17 23:57:27 -0700

    Closer but still a fail on post_order_iterative

--- Python3/binary_search_trees/post_order.py
+++ Python3/binary_search_trees/post_order.py
@@ -12,21 +12,26 @@ def post_order(curr, func=lambda x: print(x.value)):
     func(curr)
 
 
-def post_order_iterative(node, func=lambda x: print(x.value)):
-    if not node:
+def post_order_iterative(curr, func=lambda x: print(x.value)):
+    if not curr:
         return
 
-    stack = [node]
-    curr = None
-    sentinel = None
+    stack = [curr]
+    scout = curr.left
 
-    while stack:
-        curr = stack.pop()
-
-        sentinel = None
-        if stack:
-            sentinel = stack[-1]
-
-        if sentinel and sentinel.left != curr:
-            print("post order start")
+    while stack or scout:
+        if scout:
+            stack.append(scout)
+            scout = scout.left
         else:
+            curr = stack[-1].right
+
+            if not curr:
+                curr = stack.pop()
+                func(curr)
+
+                while stack and stack[-1].right != curr:
+                    curr = stack.pop()
+                    func(curr)
+            else:
+                scout = curr
