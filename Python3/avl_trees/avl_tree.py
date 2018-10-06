569d898ab7367fc6fad7603bccfdbabf364b6645
Brian Vann <brian.taylor.vann@gmail.com>
2018-10-05 23:23:16 -0700

    Start using height to balance

--- Python3/avl_trees/avl_tree.py
+++ Python3/avl_trees/avl_tree.py
@@ -51,23 +51,10 @@ class AVLTree(object):
         else:
             node.left = self._insert_rec(node.left, value)
 
-        if abs(node.height) > 1:
+        if abs(self._get_balance(node)) > 1:
             return self._tally(self._balance(node))
 
         return self._tally(node)
-
-
-    def _balance(self, node):
-        if node.height > 1:
-            if node.right.height < 0:
-                node.right = self._right(node.right)
-
-            return self._left(node)
-        else:
-            if node.left.height > 0:
-                node.left = self._left(node.left)
-
-            return self._right(node)
 
 
     def remove(self, value):
@@ -78,26 +65,71 @@ class AVLTree(object):
         pass
 
 
+    def _get_balance(self, node):
+        if not node:
+            return 0
+
+        return (
+            self._get_height(node.right)
+            - self._get_height(node.left)
+        )
+
+
+    def _balance(self, node):
+        if self._get_balance(node) < -1:
+            print("about to rotate right")
+            if self._get_balance(node.right) > 0:
+                node.left = self._left(node.left)
+
+            return self._right(node)
+        else:
+            print("about to rotate left")
+            if self._get_balance(node.right) < 0:
+                node.right = self._right(node.right)
+
+            return self._left(node)
+
+
     def _left(self, curr):
+        print("rotate left")
+
         node = curr.right
         curr.right = node.left
         node.left = curr
+
+        self._tally(node.left)
+        self._tally(node.right)
 
         return node
 
 
     def _right(self, curr):
+        print("rotate left")
+
         node = curr.left
         curr.left = node.right
         node.right = curr
 
+        self._tally(node.left)
+        self._tally(node.right)
+
         return node
 
 
+    def _get_height(self, node):
+        if not node:
+            return 0
+
+        return node.height
+
+
     def _tally(self, node):
-        if node.left:
-            node.height += node.left.height
-        if node.right:
-            node.height = max(node.right.height, node.height)
+        if not node:
+            return
+
+        node.height = max(
+            self._get_height(node.right),
+            self._get_height(node.left)
+        ) + 1
 
         return node
--- Python3/avl_trees/test_avl_tree.py
+++ Python3/avl_trees/test_avl_tree.py
@@ -22,11 +22,41 @@ class TestAVLTreeMethods(unittest.TestCase):
 
         self.assertIn(1, avl)
 
+    def test_avl_insert_one_height_zero(self):
+        avl = AVLTree(1)
+
+        self.assertEqual(avl._root.height, 1)
+
 
     def test_avl_insert_two(self):
         avl = AVLTree(1, 2)
 
         self.assertIn(2, avl)
+
+
+    def test_avl_insert_two_height_one(self):
+        avl = AVLTree(1, 2)
+
+        self.assertEqual(avl._root.height, 2)
+
+
+    def test_avl_insert_two_right_height_one(self):
+        avl = AVLTree(1, 2)
+
+        self.assertEqual(avl._root.right.height, 1)
+
+
+    def test_avl_insert_two_left_height_one(self):
+        avl = AVLTree(2, 1)
+
+        self.assertEqual(avl._root.left.height, 1)
+
+
+    def test_avl_insert_two_balance_negative_one(self):
+        avl = AVLTree(2, 1)
+
+        self.assertEqual(avl._get_balance(avl._root), -1)
+
 
     def test_avl_insert_three_adjust_one_right_heavy(self):
         avl = AVLTree(1, 2, 3)
@@ -47,9 +77,9 @@ class TestAVLTreeMethods(unittest.TestCase):
 
 
     def test_avl_insert_three_adjust_one_right_left_heavy(self):
-        avl = AVLTree(2, 3, 1)
+        avl = AVLTree(2, 4, 3)
         result = []
-        expected = [[2], [1, 3]]
+        expected = [[3], [2, 4]]
         level_order(avl._root, func=lambda x: result.append(x))
 
         self.assertEqual(result, expected)
@@ -62,59 +92,43 @@ class TestAVLTreeMethods(unittest.TestCase):
         level_order(avl._root, func=lambda x: result.append(x))
 
         self.assertEqual(result, expected)
+    #
+    #
+    #
+    #
+    #
+    #
+    # def test_avl_insert_three_root_height_two(self):
+    #     avl = AVLTree(1, 2, 3)
+    #
+    #     self.assertEqual(avl._root.height, 2)
+    #
+    #
+    # def test_avl_insert_three_right_height_one(self):
+    #     avl = AVLTree(1, 2, 3)
+    #
+    #     self.assertEqual(avl._root.right.height, 1)
+    #
+    #
+    # def test_avl_insert_three_left_height_one(self):
+    #     avl = AVLTree(3, 2, 1)
+    #
+    #     self.assertEqual(avl._root.left.height, 1)
+    #
+    #
+    #
+    #
+    # def test_avl_insert_three_rotate_left_height_zero(self):
+    #     avl = AVLTree(1, 2, 3)
+    #
+    #     self.assertEqual(avl._root.height, 2)
+    #
+    #
+    # def test_avl_insert_three_rotate_right_height_zero(self):
+    #     avl = AVLTree(3, 2, 1)
+    #
+    #     self.assertEqual(avl._root.height, 2)
 
-
-    def test_avl_insert_one_height_zero(self):
-        avl = AVLTree(1)
-
-        self.assertEqual(avl._root.height, 0)
-
-
-    def test_avl_insert_two_height_one(self):
-        avl = AVLTree(1, 2)
-
-        self.assertEqual(avl._root.height, 1)
-
-
-    def test_avl_insert_two_height_negative_one(self):
-        avl = AVLTree(2, 1)
-
-        self.assertEqual(avl._root.height, -1)
-
-    def test_avl_insert_three_rotate_left_height_zero(self):
-        avl = AVLTree(1, 2, 3)
-
-        self.assertEqual(avl._root.height, 0)
-
-
-    def test_avl_insert_three_rotate_right_height_zero(self):
-        avl = AVLTree(3, 2, 1)
-
-        self.assertEqual(avl._root.height, 0)
-
-
-    def test_avl_insert_three_left_height_zero(self):
-        avl = AVLTree(1, 2, 3)
-
-        self.assertEqual(avl._root.left.height, 0)
-
-
-    def test_avl_insert_three_right_height_zero(self):
-        avl = AVLTree(1, 2, 3)
-
-        self.assertEqual(avl._root.right.height, 0)
-
-
-    def test_avl_insert_three_left_height_zero(self):
-        avl = AVLTree(3, 2, 1)
-
-        self.assertEqual(avl._root.left.height, 0)
-
-
-    def test_avl_insert_three_right_height_zero(self):
-        avl = AVLTree(3, 2, 1)
-
-        self.assertEqual(avl._root.right.height, 0)
 
 
 if __name__ == "__main__":
